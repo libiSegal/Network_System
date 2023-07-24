a2551aaa511366dbc18e77c5c277751551fa8c1c
libiSegal <hand2b18@mbjocmp.org.il>
2023-07-24 15:25:07 +0300

    connect to DB

--- first_file.py
+++ first_file.py
@@ -1,1 +1,18 @@
-print("we start")
+import mysql.connector
+
+HOST = "sql7.freesqldatabase.com"
+USER = "sql7634893"
+PASSWORD = "8RT5Q9GPpZ"
+
+network_project_db = mysql.connector.connect(
+    host=HOST,
+    user=USER,
+    password=PASSWORD
+)
+
+mycursor = network_project_db.cursor()
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+    print(x)
