b4b9758ff4d4b967b51af7da6b21068ffae00528
libiSegal <hand2b18@mbjocmp.org.il>
2023-08-02 13:58:00 +0300

    add logger

--- moduls/network_visualization.py
+++ moduls/network_visualization.py
@@ -1,20 +1,17 @@
-
 import networkx as nx
 from moduls import network_handle
 import matplotlib.pyplot as plt
+from log_file import logger
 
 
+@logger
 def get_visual(network_id):
-    print(network_id)
     network_data = network_handle.get_network_data(network_id)
     if not network_data:
         raise Exception('No data for this network')
-    print(network_data)
     plt.clf()
     edges_list = network_data['communication']
     network_graf = nx.DiGraph()
     network_graf.add_edges_from(edges_list)
     nx.draw_spring(network_graf, with_labels=True)
     plt.savefig('graf.png', format='png')
-
-
