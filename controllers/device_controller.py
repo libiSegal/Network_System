a4821c2a7b54741dec6431116a1defebb54d6bb2
Avigail <bm0777676818@gmail.com>
2023-08-02 09:33:20 +0300

    device_controller

--- controllers/device_controller.py
+++ controllers/device_controller.py
@@ -1,15 +1,17 @@
 
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter
 from moduls import devices_handle
-from moduls import security
+
 device_router = APIRouter()
 
 
 @device_router.get("/network/{network_id}/devices/")
-async def get_devices(network_id,current_user: security.User = Depends(security.get_current_active_user)):
+async def get_devices(network_id):
     return devices_handle.get_all_devices(network_id)
 
 
 @device_router.get("/{client_id}/devices/")
-async def get_network_data(client_id,current_user: security.User = Depends(security.get_current_active_user)):
+async def get_network_data(client_id):
     return devices_handle.get_devices_by_client_id(client_id)
+
+
