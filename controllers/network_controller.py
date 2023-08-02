30887f8184d92080b6628e528bd491ea7ae4fe0b
Avigail <bm0777676818@gmail.com>
2023-08-02 14:05:07 +0300

    add_exception

--- controllers/network_controller.py
+++ controllers/network_controller.py
@@ -4,7 +4,7 @@ from moduls import network_handle
 from moduls import communication_handle
 from moduls import network_visualization
 from moduls import security
-from fastapi import APIRouter, UploadFile, File, Form, Depends
+from fastapi import APIRouter, UploadFile, File, Form, Depends, HTTPException
 from fastapi.responses import FileResponse
 import io
 import PIL.Image as Image
@@ -21,7 +21,7 @@ async def upload_pcap_file(file: UploadFile = File(...),
                            ):
     # Check if a file was provided in the request
     if not file:
-        return 'No file uploaded', 400
+        raise HTTPException(400, 'No file uploaded')
     network_handle.create_network(file, client_id, location, technician_id)
     return f'File{file.filename} uploaded successfully'
 
@@ -29,15 +29,21 @@ async def upload_pcap_file(file: UploadFile = File(...),
 @network_router.get("/{network_id}/communication")
 async def get_network_communication(network_id,
                                     current_user: security.User = Depends(security.get_current_active_user)):
+    if type(network_id) != int:
+        raise HTTPException(400, 'Invalid input')
     return communication_handle.get_communication(network_id)
 
 
 @network_router.get("/{network_id}")
 async def get_network_details(network_id):
+    if type(network_id) != int:
+        raise HTTPException(400, 'Invalid input')
     return network_handle.get_network_data(network_id)
 
 
 @network_router.get('/{network_id}/visual')
-async def a(network_id):
+async def visual(network_id):
+    if type(network_id) != int:
+        raise HTTPException(400, 'Invalid input')
     network_visualization.get_visual(network_id)
     return FileResponse('graf.png')
