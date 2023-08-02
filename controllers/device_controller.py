214b8ee93bde502bb1e32b29916497c652641e2e
Avigail <bm0777676818@gmail.com>
2023-08-02 13:46:38 +0300

    add_exception_for_device_controller

--- controllers/device_controller.py
+++ controllers/device_controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from moduls import devices_handle
 from moduls import security
 
@@ -7,11 +7,13 @@ device_router = APIRouter()
 
 @device_router.get("/network/{network_id}/devices/")
 async def get_devices(network_id, current_user: security.User = Depends(security.get_current_active_user)):
+    if type(network_id) != int:
+        raise HTTPException(400, 'Invalid input')
     return devices_handle.get_all_devices(network_id)
 
 
 @device_router.get("/{client_id}/devices/")
 async def get_network_data(client_id, current_user: security.User = Depends(security.get_current_active_user)):
+    if type(client_id) != int:
+        raise HTTPException(400, 'Invalid input')
     return devices_handle.get_devices_by_client_id(client_id)
-
-
